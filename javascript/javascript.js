db3972268fc671f223b51330c8d021525889a90c
DmalTzi <Audyomg@gmail.com>
2022-01-30 22:16:35 +0700

    test

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,13 @@
 function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
 }
+
+const countEL = document.getElementById('count');
+
+function updateVisitCount(){
+    fetch('https://api.countapi.xyz/create?namespace=florin-pop&key=youtube&value=0')
+        .then(res => res.json())
+        .then(res => {
+            countEL.innerHTML = res.value
+        });
+}
